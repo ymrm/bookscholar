63618602f4ac20863da5d97c45e0fe913459e6c9
ymrm <s1511514@u.tsukuba.ac.jp>
2018-10-29 19:31:42 +0900

    新書本を選択したら54の学問がランキング順に表示されるようになった

--- test2.php
+++ test2.php
@@ -9,13 +9,19 @@ echo $post_book_id;
 <body>
 <?php
 $db=new SQLite3('./database.db');
-for ($i=0; $i<793;$i++){
+for ($i=1; $i<55;$i++){
+  $sql_result=$db->query("SELECT * FROM rank WHERE book_id = $post_book_id and rank = $i");
+  $data=$sql_result->fetchArray();
+  $book_id = $data['book_id'];
+  $sch_id = $data['sch_id'];
+  $rank = $data['rank'];
+  print "<p>No.".$rank."</p>";
+  //print "<p>ID.".$sch_id."</p>";
 
-
-$sql_result=$db->query("SELECT * FROM title WHERE book_id = $i");
-$data=$sql_result->fetchArray();
-$book_id = $data['book_id'];
-$title = $data['title'];
+  $sql_result=$db->query("SELECT * FROM scholar WHERE sch_id = $sch_id");
+  $data=$sql_result->fetchArray();
+  $sch_name = $data['sch_name'];
+  print "<p>".$sch_name."</p>";
 print "</br>";
 }
 $db->close();
