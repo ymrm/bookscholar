438b05737719f2edbf3ee99fb3d496e7ba214905
ymrm <s1511514@u.tsukuba.ac.jp>
2018-10-29 18:09:34 +0900

    全件のタイトルが表示されるようになった

--- test.php
+++ test.php
@@ -3,12 +3,17 @@
 <title>aaaa</title>
 </head>
 <body>
-<p>cccc</p>
 <?php
 $db=new SQLite3('./database.db');
-$sql_result=$db->query("SELECT * FROM title");
+for ($i=0; $i<793;$i++){
+
+
+$sql_result=$db->query("SELECT * FROM title WHERE book_id = $i");
 $data=$sql_result->fetchArray();
-print $data["book_id"]." : ".$data["title"];
+//print $data["book_id"]." : ".$data["title"];
+print '<a href="http://google.co.jp">'.$data['title'].'</a>';
+print "</br>";
+}
 $db->close();
 ?>
 </body>
