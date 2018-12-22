371c4764e5f353f809ad661f0d2f3675445b3cb7
ymrm <s1511514@u.tsukuba.ac.jp>
2018-12-22 17:58:53 +0900

    最後の新書本だけ学問ページに遷移しなかったので修正

--- test.php
+++ test.php
@@ -86,7 +86,7 @@ $content = $data_1['content'];
 $summary_content = $summary.$content;
 
 
-print '<form method="post" name="form1" action="test2.php">';
+print '<form method="post" name="form1" action="keyword_test2.php">';
 // 次のページに選択した新書本のIDを送る
 print '<input type="hidden" name="book_id" value="';
 echo $book_id; 
@@ -137,7 +137,7 @@ $summary_content = $summary.$content;
 
 
 
-print '<form method="post" name="form1" action="test2.php">';
+print '<form method="post" name="form1" action="keyword_test2.php">';
 // 次のページに選択した新書本のIDを送る
 print '<input type="hidden" name="book_id" value="';
 echo $book_id; 
@@ -165,65 +165,13 @@ print '</div>';
   <div class="swiper-container">
     <div class="swiper-wrapper">
 <?php
-for ($j=0; $j<792;$j+=12){  //全ての新書本を表示
+for ($j=0; $j<793;$j+=12){  //全ての新書本を表示
 		print '<div class="swiper-slide">';
 		outputsinsyo($j);
 		print '</div>';
 }
 ?>
       <div class="swiper-slide">
-<?php
-print '<div id="wrap">';
-print '<div id="left_content">';
-$db=new SQLite3('./database.db');
-
-$sql_result=$db->query("SELECT * FROM title WHERE book_id = 792");
-$data=$sql_result->fetchArray();
-$book_id = $data['book_id'];
-$title = $data['title'];
-
-
-//ISSNの表示
-$sql_result_3=$db->query("SELECT * FROM isbn WHERE book_id = 792");
-$data_3=$sql_result_3->fetchArray();
-$isbn = $data_3['isbn'];
-//print "<p>".$isbn."</p>";
-
-//内容説明の表示
-$sql_result_2=$db->query("SELECT * FROM summary WHERE book_id = 792");
-$data_2=$sql_result_2->fetchArray();
-$summary = $data_2['summary'];
-//print "<p>".$summary."</p>";
-
-//目次の表示
-$sql_result_1=$db->query("SELECT * FROM content WHERE book_id = 792");
-$data_1=$sql_result_1->fetchArray();
-$content = $data_1['content'];
-//print "<p>".$content."</p>";
-
-$summary_content = $summary.$content;
-
-
-print '<form method="post" name="form1" action="test2.php">';
-// 次のページに選択した新書本のIDを送る
-print '<input type="hidden" name="book_id" value="';
-echo $book_id; 
-print '">';
-print '<div class="box2">';
-print '<a href="javascript:form1[';
-echo $i;
-print '].submit()">';
-print '<span title='.$summary_content.'>'.$title.'</p>';
-print '</a>';
-print '</div>';
-
-
-
-print "</form>";
-print "</br>";
-print '</div>';
-print '</div>';
-?>
 </div>
     </div>
     <!-- Add Arrows -->
